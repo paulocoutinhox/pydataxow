d9278002d2c6ccc625b2b7d824c5b23d8ccaf9f4
“Paulo <paulo@ubook.com>
2024-02-06 00:53:07 -0300

    project updated

--- src/backend/modules/config.py
+++ src/backend/modules/config.py
@@ -1,14 +1,18 @@
 import os
+import sys
 
 # window
 is_debug = True if os.environ.get("PYWEBVIEW_DEBUG") == "1" else False
 
-# development path
-root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", ".."))
-
-# frozen executable path
-if not os.path.exists(root_dir):
-    root_dir = os.path.dirname(os.path.abspath(__file__))
+# root path
+if getattr(sys, "frozen", False):
+    # frozen executable path
+    root_dir = os.path.join(sys._MEIPASS)
+else:
+    # normal path
+    root_dir = os.path.abspath(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "..")
+    )
 
 # gui
 gui_panel_dir = os.path.join(root_dir, "gui", "panel")
