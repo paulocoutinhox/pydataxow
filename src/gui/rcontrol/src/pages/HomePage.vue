0a291d4b5a0921b57ee4f41988d861e46cb39922
“Paulo <paulo@ubook.com>
2024-02-07 00:01:13 -0300

    project updated

--- src/gui/rcontrol/src/pages/HomePage.vue
+++ src/gui/rcontrol/src/pages/HomePage.vue
@@ -1,12 +1,14 @@
 <script setup>
 import axios from "axios";
-import { ref, watch } from 'vue';
+import { nextTick, ref, watch } from 'vue';
 import PageHeader from '../components/PageHeader.vue';
+import PageFooter from '../components/PageFooter.vue';
 import useSharedState from '../states/app-state';
 
 const { systemReady, apiUrl } = useSharedState();
 
 const playerText = ref("Jesus é bom demais! É isso aí povão! Deus te abençoe!");
+const btPlayerClearText = ref(null);
 
 watch([systemReady], ([value]) => {
     if (value) {
@@ -64,23 +66,32 @@ function playerSetText() {
         'text': playerText.value
     });
 }
+
+function playerClearText() {
+    playerText.value = "";
+
+    nextTick(() => {
+        btPlayerClearText.value.focus();
+    });
+}
+
+function playerSetImage() {
+    playerUpdateData({ type: 'image', src: 'https://picsum.photos/1024/768/?blur=2&rnd=' + new Date().getTime() });
+}
+
+function playerSetVideo() {
+    playerUpdateData({ type: 'video', src: 'https://sample-videos.com/video321/mp4/720/big_buck_bunny_720p_1mb.mp4' });
+}
+
+function playerClear() {
+    playerUpdateData({ type: 'black' });
+}
 </script>
 
 <template>
     <div v-if="systemReady">
         <div class="container">
             <PageHeader />
-
-            <div class="row g-3">
-                <h3>Player - Text</h3>
-                <div class="mb-3">
-                    <label for="inputNewText" class="form-label">New Text</label>
-                    <input :value="playerText" type="email" class="form-control" id="inputNewText">
-                </div>
-                <button type="button" class="btn btn-primary" @click="playerSetText">Send</button>
-            </div>
-
-            <hr>
 
             <div class="row g-3">
                 <h3>Player - Control</h3>
@@ -90,7 +101,46 @@ function playerSetText() {
                 <div class="col-auto">
                     <button type="button" class="btn btn-primary" @click="playerHide">Hide Player</button>
                 </div>
+                <div class="col-auto">
+                    <button type="button" class="btn btn-primary" @click="playerClear">Clear Player</button>
+                </div>
             </div>
+
+            <hr>
+
+            <div class="row g-3">
+                <h3>Player - Text</h3>
+                <div class="mb-3">
+                    <label for="inputNewText" class="form-label">New Text</label>
+                    <input v-model="playerText" type="email" class="form-control" id="inputNewText" ref="btPlayerClearText">
+                </div>
+                <div class="col-auto">
+                    <button type="button" class="btn btn-primary" @click="playerSetText">Send</button>
+                </div>
+                <div class="col-auto">
+                    <button type="button" class="btn btn-primary" @click="playerClearText">Clear</button>
+                </div>
+            </div>
+
+            <hr>
+
+            <div class="row g-3">
+                <h3>Player - Image</h3>
+                <div class="col-auto">
+                    <button type="button" class="btn btn-primary" @click="playerSetImage">Send</button>
+                </div>
+            </div>
+
+            <hr>
+
+            <div class="row g-3">
+                <h3>Player - Video</h3>
+                <div class="col-auto">
+                    <button type="button" class="btn btn-primary" @click="playerSetVideo">Send</button>
+                </div>
+            </div>
+
+            <PageFooter />
         </div>
     </div>
 </template>
--- src/gui/rcontrol/vue.config.js
+++ src/gui/rcontrol/vue.config.js
@@ -1,5 +1,5 @@
 const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
-    publicPath: isProduction ? '/rcontrol/' : '/'
+    publicPath: isProduction ? '/rcontrol/' : '/',
 }
